50cea1201a94e9340ac67f1f4667d71f554633e5
pistocop <simone.guardati42@gmail.com>
2023-11-09 17:46:55 +0100

    add vscode settings, add port to serve app, remove useless router

--- {{cookiecutter.project_slug}}/scripts/serve-app.sh
+++ {{cookiecutter.project_slug}}/scripts/serve-app.sh
@@ -6,8 +6,10 @@ set -x
 ACTUAL_DIR=${0%/*}
 cd "${ACTUAL_DIR}"/../
 
+PORT=${1:-8080}
+
 # Launch using uvicorn:
-uvicorn --reload --host=0.0.0.0 --port=8080 app.main:app
+uvicorn --reload --host=0.0.0.0 --port="$PORT" app.main:app
 
 # Launch using python `main.py`:
 # export PYTHONPATH="$PWD" && python app/main.py
